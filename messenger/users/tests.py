35a1b9bc92939bd718471a259acfbd8a56b5627a
Татьяна Алексеевна Мельникова <tanya@localhost.localdomain>
2020-04-19 23:01:37 +0300

    1

--- messenger/users/tests.py
+++ messenger/users/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import json
+from django.test import TestCase, Client
+from users.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class UsersTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create(username="test_user_1")
+        self.client.force_login(self.user)
+
+    def test_search_user(self):
+        response = self.client.get(reverse('profile', kwargs={}))
+
